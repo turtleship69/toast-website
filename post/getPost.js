93ef081128230cef1d87387afbd95a3555115874
Nathan Machane <73017570+turtleship69@users.noreply.github.com>
2023-12-19 17:41:57 +0000

    add conditional rendering and time context

--- post/getPost.js
+++ post/getPost.js
@@ -105,16 +105,20 @@ function createPostHTML(post) {
     var mediaContainer = document.createElement("div");
     mediaContainer.className = "posts post media container";
 
-    var postImage = document.createElement("img");
-    postImage.className = "posts post media image";
-    postImage.src = post.Image1;
-    mediaContainer.appendChild(postImage);
 
-    var caption = document.createElement("p");
-    caption.className = "posts post media caption";
-    caption.innerText = post.Body;
-    mediaContainer.appendChild(caption);
+    if (post.Image1) {
+        var postImage = document.createElement("img");
+        postImage.className = "posts post media image";
+        postImage.src = post.Image1;
+        mediaContainer.appendChild(postImage);
 
+        if (post.Body) {
+            var caption = document.createElement("p");
+            caption.className = "posts post media caption";
+            caption.innerText = post.Body;
+            mediaContainer.appendChild(caption);
+        }
+    }
     postDiv.appendChild(mediaContainer);
 
     // Actions container
@@ -158,9 +162,36 @@ function createPostHTML(post) {
 
 // Helper function to calculate time ago
 function calculateTimeAgo(uploadTime) {
-    // Implement your logic to calculate time ago based on the given UploadTime
-    // This is just a placeholder, you may want to use a library like moment.js for a more robust solution
-    return "12 hours ago";
+    // work out time ago from unix timestamp 
+    const now = new Date();
+    const uploadDate = new Date(uploadTime * 1000);
+    console.log(now, uploadDate);
+    const timeDiff = now - uploadDate;
+
+    const seconds = Math.floor(timeDiff / 1000);
+    const minutes = Math.floor(seconds / 60);
+    const hours = Math.floor(minutes / 60);
+    const days = Math.floor(hours / 24);
+    const months = Math.floor(days / 30);
+    const years = Math.floor(months / 12);
+    console.log(years, months, days, hours, minutes, seconds);
+
+    let timeAgo = "";
+    if (years > 0) {
+        timeAgo = `${years} year${years > 1 ? "s" : ""} ago`;
+    } else if (months > 0) {
+        timeAgo = `${months} month${months > 1 ? "s" : ""} ago`;
+    } else if (days > 0) {
+        timeAgo = `${days} day${days > 1 ? "s" : ""} ago`;
+    } else if (hours > 0) {
+        timeAgo = `${hours} hour${hours > 1 ? "s" : ""} ago`;
+    } else if (minutes > 0) {
+        timeAgo = `${minutes} minute${minutes > 1 ? "s" : ""} ago`;
+    } else {
+        timeAgo = `${seconds} second${seconds > 1 ? "s" : ""} ago`;
+    }
+
+    return timeAgo; // return the time ago string in the format "1 year ago" or "1 month ago" or "1 day ago" or "1 hour ago" or "1 minute ago" or "1 second ago" or "1 second ago" or "1 second ago" or "1
 }
 
 
