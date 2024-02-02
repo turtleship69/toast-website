5948c9eeaccdeb22fbc3f53d2c765a6f4511b464
Nathan Machane <73017570+turtleship69@users.noreply.github.com>
2024-02-02 18:29:02 +0000

    build script for netlify

---
+++ .netlify/build.py
@@ -0,0 +1,26 @@
+import json
+import os
+"""this file gets the environment variables and saves them to config files"""
+
+class Config():
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
+        self.variables = []
+
+    def add_variable(self, name: str, value: str) -> None:
+        self.variables.append([name, value])
+
+    def save(self: str) -> None:
+        #open self.filename and write the variables to it
+        config = {}
+        for item in self.variables:
+            config[item[0]] = item[1]
+
+        with open(f"config/{self.filename}.json", 'w') as f:
+            json.dump(config, f)
+
+
+hanko = Config("hanko")
+# get HANKO_URL and save it as url
+hanko.add_variable("url", os.environ["HANKO_URL"])
+hanko.save()
