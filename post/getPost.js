8f32cf08cfaaf61576bd41e134d9ef35de5f4b19
Nathan Machane <73017570+turtleship69@users.noreply.github.com>
2024-02-03 17:26:01 +0000

    small refactorings

--- post/getPost.js
+++ post/getPost.js
@@ -73,7 +73,7 @@ function createPostHTML(post) {
 
     var username = document.createElement("p");
     username.className = "posts post user details username";
-    username.innerText = post.Username; // Update with actual username
+    username.innerText = post.Username; 
     userDetails.appendChild(username);
 
     var context = document.createElement("p");
@@ -96,6 +96,7 @@ function createPostHTML(post) {
 
     postDiv.appendChild(titleContainer);
 
+
     // Media container
     var mediaContainer = document.createElement("div");
     mediaContainer.className = "posts post media container";
@@ -113,6 +114,14 @@ function createPostHTML(post) {
             caption.innerText = post.Body;
             mediaContainer.appendChild(caption);
         }
+    } else if (post.Body) {
+        var postTextDiv = document.createElement("div");
+        postTextDiv.className = "posts post content container";
+        var postText = document.createElement("p");
+        postText.className = "posts post content text";
+        postText.innerText = post.Body;
+        postTextDiv.appendChild(postText);
+        postDiv.appendChild(postTextDiv);
     }
     postDiv.appendChild(mediaContainer);
 
@@ -154,39 +163,3 @@ function createPostHTML(post) {
 
     return postDiv;
 }
-
-// Helper function to calculate time ago
-function calculateTimeAgo(uploadTime) {
-    // work out time ago from unix timestamp 
-    const now = new Date();
-    const uploadDate = new Date(uploadTime * 1000);
-    console.log(now, uploadDate);
-    const timeDiff = now - uploadDate;
-
-    const seconds = Math.floor(timeDiff / 1000);
-    const minutes = Math.floor(seconds / 60);
-    const hours = Math.floor(minutes / 60);
-    const days = Math.floor(hours / 24);
-    const months = Math.floor(days / 30);
-    const years = Math.floor(months / 12);
-    console.log(years, months, days, hours, minutes, seconds);
-
-    let timeAgo = "";
-    if (years > 0) {
-        timeAgo = `${years} year${years > 1 ? "s" : ""} ago`;
-    } else if (months > 0) {
-        timeAgo = `${months} month${months > 1 ? "s" : ""} ago`;
-    } else if (days > 0) {
-        timeAgo = `${days} day${days > 1 ? "s" : ""} ago`;
-    } else if (hours > 0) {
-        timeAgo = `${hours} hour${hours > 1 ? "s" : ""} ago`;
-    } else if (minutes > 0) {
-        timeAgo = `${minutes} minute${minutes > 1 ? "s" : ""} ago`;
-    } else {
-        timeAgo = `${seconds} second${seconds > 1 ? "s" : ""} ago`;
-    }
-
-    return timeAgo; // return the time ago string in the format "1 year ago" or "1 month ago" or "1 day ago" or "1 hour ago" or "1 minute ago" or "1 second ago" or "1 second ago" or "1 second ago" or "1
-}
-
-
