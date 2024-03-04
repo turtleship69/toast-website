bc7fabb12f705fca069d35d787ed8516922978cd
Nathan Machane <73017570+turtleship69@users.noreply.github.com>
2024-03-04 09:53:42 +0000

    update error handling

--- post/getPost.js
+++ post/getPost.js
@@ -6,8 +6,9 @@ if (postId) {
 
     fetch(apiUrl)
         .then(response => {
-            if (response.status === 401) {
-                return response.json().then(data => {
+            console.log(response.status)
+            return response.json().then(data => {
+                if (response.status === 401) {
                     if (data.message === "Invalid session") {
                         // Redirect to the login page
                         login_redirect_url = `/login/?redirect_url=/post/?p=${new URLSearchParams(window.location.search).get('p')}`
@@ -15,21 +16,16 @@ if (postId) {
                     } else {
                         throw new Error(`Unauthorized: ${data.message}`);
                     }
-                });
-            } else if (response.status === 400 || response.status === 404) {
-                return response.json().then(data => {
-                    if (data.message === "Invalid post ID") {
-                        errorMessage = document.createElement("p")
-                        errorMessage.innerText = "Post not found"
-                        document.getElementById("posts").appendChild(errorMessage)
-                    } else {
-                        throw new Error(`Unauthorized: ${data.message}`);
-                    }
-                });
-            } else if (!response.ok) {
-                throw new Error(`Error: ${response.status}`);
-            }
-            return response.json();
+                } else if (response.status != 200) {
+                    console.error("Error fetching data:", data.message)
+                    errorMessage = document.createElement("p")
+                    errorMessage.innerText = data.message
+                    document.getElementById("posts").appendChild(errorMessage)
+                } else if (!response.ok) {
+                    throw new Error(`Error: ${response.status}`);
+                }
+                return response.json();
+            });
         })
         .then(data => {
             let post = data;
@@ -40,7 +36,6 @@ if (postId) {
             var postContainer = document.getElementById("posts");
             postContainer.appendChild(postHTML); // Append the generated post HTML to the body or any other desired element
         })
-        .catch(error => console.error("Error fetching data:", error));
 } else {
     errorMessage = document.createElement("p")
     errorMessage.innerText = "No post ID provided"
