4c9e5f02b1ce136916cda456c4fd724cc1a7f44b
Nathan Machane <73017570+turtleship69@users.noreply.github.com>
2024-04-11 12:30:38 +0100

    update text about passkeys to be less intimidating

--- login/login.js
+++ login/login.js
@@ -1,6 +1,12 @@
 import { register } from "https://esm.run/@teamhanko/hanko-elements";
+import { en } from "https://esm.run/@teamhanko/hanko-elements/i18n/en";
 
-var login_url = "/hanko/login" 
+en.texts.setupPasskey = "Passkeys are a secure and convenient way to sign in" +
+    ", using your device's fingerprint or PIN instead of a password. " +
+    "Passkeys are stored securely on your device itself, makes signing in "+ 
+    "faster and safer than ever."
+
+var login_url = "/hanko/login"
 const redirect_url = new URLSearchParams(window.location.search).get('redirect_url')
 if (redirect_url) {
     login_url += "?redirect_url=" + redirect_url;
@@ -9,7 +15,7 @@ if (redirect_url) {
 // Define an async function to use the await keyword
 async function fetchData() {
     try {
-        const { hanko } = await register(hanko_config.url);
+        const { hanko } = await register(hanko_config.url, { translations: { en } });
 
         hanko.onAuthFlowCompleted(async () => {
             // successfully logged in, make a request to "/api/login"
@@ -25,8 +31,6 @@ async function fetchData() {
         // Handle errors during the fetch process or other asynchronous operations
         console.error('Error:', error);
     }
-
-
 }
 
 // Call the async function
