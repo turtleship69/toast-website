deb2b6ed57fc17a058b99301f2631d5d0cd45253
Nathan Machane <73017570+turtleship69@users.noreply.github.com>
2024-05-05 19:22:14 +0100

    🐒

--- netlify/hanko.py
+++ netlify/hanko.py
@@ -2,4 +2,7 @@ import os, json
 
 def save_config():
     with open(f"config/hanko.js", 'w') as f:
-        f.write(f"const hanko_config = {json.dumps({'url', os.environ['HANKO_URL']}, indent=4)}")
+        config = {
+            'url': os.environ['HANKO_URL']
+        }
+        f.write(f"const hanko_config = {json.dumps({config}, indent=4)}")
