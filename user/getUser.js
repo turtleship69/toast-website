7a0f47dffa8b46c8e0877b6e9d9bd0ccb85a6744
Nathan Machane <73017570+turtleship69@users.noreply.github.com>
2024-05-05 18:24:50 +0100

    meta and friends

--- user/getUser.js
+++ user/getUser.js
@@ -15,24 +15,31 @@ if (!current_user) {
             //if the user exists, display their info
             if (data.status == "success") {
                 addBreadcrumbUser(data)
-                addMetaTags(data)
-
-
+                
+                
                 document.getElementById('loading').style.display = 'none'
                 document.getElementById('main').style.display = 'block'
-
+                
                 document.getElementById('username').innerHTML = current_user
+                document.title = '@' + current_user + ' | Toast'
                 followers = data.followers
                 document.getElementById('follower-count').innerHTML = formatNumber(followers)
                 following = data.following
                 document.getElementById('following-count').innerHTML = formatNumber(following)
-                document.getElementById('bio').innerHTML = data.bio
+                if (!data.bio) {
+                    document.getElementById('bio').innerHTML = "@" + data.username
+                    data.bio = "See @" + data.username + "'s profile on toast"
+                } else {
+                    document.getElementById('bio').innerHTML = data.bio
+                }
+                //set meta description to user bio
+                addMetaTags(data)
                 if (data.gravatar) {
                     document.getElementById('profile-picture').src = data.gravatar
                 } else {
                     document.getElementById('profile-picture').src = '/placeholder/pfp.png'
                 }
-                
+
                 //if there are no posts, make #no-posts visible
                 if (data.posts.length == 0) {
                     document.getElementById('no-posts').style.display = 'block'
@@ -56,7 +63,10 @@ if (!current_user) {
                     removeFollowButton
                     FriendRequestSentButton()
                     follow_button.innerHTML = "Friends"
+                    follow_button.style.backgroundColor = '#9A58C6'
                     console.log("friends")
+                } else {
+                    console.log(data.friendship)
                 }
             } else {
                 //if the user doesn't exist, display an error message
@@ -67,15 +77,15 @@ if (!current_user) {
 
 
 
-function followEvent(){follow(current_user)}
-function friendRequestEvent(){friendRequest(current_user)}
-function unfollowEvent(){unfollow(current_user)}
+function followEvent() { follow(current_user) }
+function friendRequestEvent() { friendRequest(current_user) }
+function unfollowEvent() { unfollow(current_user) }
 
 function addFollowButton() {
     //make the button say "follow", remove any current event listeners and add one that runs follow()
     follow_button.innerHTML = "Follow"
     follow_button.style.backgroundColor = "#31B1EA"
-    
+
     follow_button.removeEventListener('click', unfollowEvent);
     follow_button.addEventListener('click', followEvent);
 }
@@ -84,7 +94,7 @@ function removeFollowButton() {
     //make the button say "unfollow", remove any current event listeners and add one that runs unfollow()
     follow_button.innerHTML = 'Following'
     follow_button.style.backgroundColor = '#2ecc71'
-    
+
     follow_button.removeEventListener('click', followEvent);
     follow_button.addEventListener('click', unfollowEvent);
 }
@@ -109,12 +119,12 @@ function FriendRequestSentButton() {
     friend_request_button.addEventListener('click', unfollowEvent);
 }
 
-function alreadyFriended(){
+function alreadyFriended() {
     //hide the friend request button
     friend_request_button.style.display = 'none'
 }
 
-setTimeout(function(){isUserProfile = true}, 100);
+setTimeout(function () { isUserProfile = true }, 100);
 
 
 function addBreadcrumbUser(data) {
